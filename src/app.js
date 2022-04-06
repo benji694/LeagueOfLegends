52935986a95ed8be735bb3f4784045df96cf2cdc
benji694 <bbouillard@edenschool.fr>
2022-04-06 11:31:33 +0200

    fetch api done

--- src/app.js
+++ src/app.js
@@ -1,4 +1,23 @@
+const champions = document.querySelector("#champions");
+
 fetch("http://ddragon.leagueoflegends.com/cdn/12.5.1/data/fr_FR/champion.json")
   .then((response) => response.json())
-  .then((data) => console.log(data.data))
+  .then((data) => {
+    let api = data.data;
+    console.log(api);
+    for (const property in api) {
+      let element = document.createElement("li");
+      let img = new Image();
+      let name = document.createElement("p");
+      img.src =
+        "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" +
+        api[property].name +
+        "_0.jpg";
+      name.textContent = api[property].name;
+      element.appendChild(img);
+      element.appendChild(name);
+      champions.appendChild(element);
+      console.log(`${property}: ${api[property]}`);
+    }
+  })
   .catch((error) => console.log(error));
